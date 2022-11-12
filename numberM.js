176bdb94a0727b42f257e0df50b96579907c1be6
Catalina Simonovich <catalinasimonovich1@gmail.com>
2022-11-12 19:34:32 -0300

    :sparkles:Feat: Add content to numberM JS

--- numberM.js
+++ numberM.js
@@ -4,3 +4,16 @@ console.log("parseInt() function:");
 const num = "65";
 console.log(num);
 console.log(parseInt(num));
+
+//parseFloat(): is a function parses a string argument and returns a floating point number
+console.log("parseFloat() function:");
+console.log("0.0314E+2");
+console.log(parseFloat("0.0314E+2"));
+
+//Number(): converts a string in a number type
+console.log("Number() function:");
+const numb = "5535.259";
+console.log(numb);
+console.log(Number(numb));
+
+//isNaN():
